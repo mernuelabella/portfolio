0b0544096e999f68f2719138f59f06e1c913449c
Mernuel Abella <mernyboi@gmail.com>
2025-09-16 18:41:26 +0800

    Add react router basic codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>Test</div>
-    </>
+    <Routes>
+      <Route index element={<div>Homepage</div>} />
+      <Route path='work' element={<div>Work page</div>} />
+    </Routes>
   )
 }
 
