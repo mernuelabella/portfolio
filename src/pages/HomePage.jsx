427ca0cf158e926089ec8b7afe8f8c4985fb8d29
Mernuel Abella <mernyboi@gmail.com>
2025-09-19 16:11:28 +0800

    add responsive styles on header

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+export function HomePage() {
+    return (
+        <>
+            <Header />
+            
+            Homepage Test
+        </>
+    )
+}
