5e4b05196c3810bffa6b9652dbf52fbf4dc2cad8
Mernuel Abella <mernyboi@gmail.com>
2026-01-09 15:48:29 +0800

    Add styles on homepage and cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import './HomePage.css'
+
 export function HomePage() {
     return (
         <>
-            Homepage Test
+            <div className="homepage page-width">
+                <div className="container border-thin">
+                    <p>
+                        I’m a Shopify-focused developer with over 10 years of experience building and customizing Shopify stores for a wide range of businesses. My expertise is primarily in theme development, custom storefront features, and performance optimization using Liquid, HTML, CSS, JavaScript, and Shopify’s core APIs.
+                    </p>
+                </div>
+                <div className="container border-thin">
+                    <p>
+                        I specialize in turning business requirements into clean, reliable, and maintainable Shopify solutions. Most of my work has been hands-on with theme customization, CSS-driven styling and layout refinement, UX improvements, and extending Shopify’s native functionality to create smooth, user-friendly shopping experiences. I value clear communication, thoughtful problem-solving, and long-term maintainability in every project.
+                    </p>
+                </div>
+                <div className="container border-thin">
+                    <p>
+                        While my professional experience is mainly focused on themes and storefront development, I am currently studying Shopify app development to expand my skill set and stay aligned with Shopify’s evolving ecosystem.
+                    </p>
+                </div>
+                
+            </div>
         </>
     )
 }
